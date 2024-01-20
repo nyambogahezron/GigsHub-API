250b3b9b984715ae4026e33ade4aaf60cab7b5e0
Hezron Nyamboga <hezzyn1@gmail.com>
2024-01-20 14:09:56 +0300

    user routes - controller setup

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const { registerUser, loginUser, logoutUser } = require('../controllers/authController')
+
+router.post('/register', registerUser )
+router.post('/login', loginUser )
+router.post('/logout', logoutUser )
+
+module.exports = router
